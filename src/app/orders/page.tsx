f42ecc17f2b85a54b9494dfc3f8ccb0c76b93526
ilyas182 <ilyas2104@gmail.com>
2025-01-08 11:45:14 +0800

    minor font changes

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -81,7 +81,7 @@ export default function Orders() {
               placeholder="Search"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="pl-10 pr-4 py-2 border rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-purple-500 text-black"
+              className="pl-10 pr-4 py-2 border rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-purple-500 text-black text-sm"
             />
             <div className="absolute left-3 top-1/2 -translate-y-1/2">
               <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -93,7 +93,7 @@ export default function Orders() {
           {/* Filters Button */}
           <button 
             onClick={() => setShowFilters(!showFilters)}
-            className="flex items-center gap-2 px-4 py-2 border rounded-md bg-white hover:bg-gray-50 text-black"
+            className="flex items-center gap-2 px-4 py-2 border rounded-md bg-white hover:bg-gray-50 text-black text-sm"
           >
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
@@ -103,7 +103,7 @@ export default function Orders() {
   
           {/* Order Summary Button */}
           <button 
-          className="flex items-center gap-2 px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-600"
+          className="flex items-center gap-2 px-4 py-2 bg-purple-500 text-white text-sm rounded-md hover:bg-purple-600"
           onClick={()=>handleOrderSummary()}
           >
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
