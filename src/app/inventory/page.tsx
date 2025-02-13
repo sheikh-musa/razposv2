438ce9015e3b0bb17140666864e27580f8c3abb6
ilyas182 <ilyas2104@gmail.com>
2025-02-13 10:20:48 +0800

    design tweaks restore button

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -361,7 +361,7 @@ export default function Inventory() {
                                     </svg>
                                     Edit
                                 </button>
-                                <button 
+                                {!showDeletedItems ? <button 
                                     className="flex items-center gap-2 px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full"
                                     onClick={() => {
                                         // Handle delete
@@ -373,7 +373,23 @@ export default function Inventory() {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                     </svg>
                                     Delete
+                                </button> : 
+                                
+                                    <button 
+                                    className="flex items-center gap-2 px-4 py-2 text-sm text-purple-600 hover:bg-gray-100 w-full"
+                                    onClick={() => {
+                                        // Handle delete
+                                        setShowMultiRestoreModal(true);
+                                        setShowOptions(false);
+                                    }}
+                                >
+                                    <svg className="w-4 h-4 text-purple-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                                    </svg>
+                                    Restore
                                 </button>
+                                }
+                                
                             </div>
                         )}
                     </div>
