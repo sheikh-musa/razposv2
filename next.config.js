beaf9f78d966996985285624c09f0ee05141db41
ilyas182 <ilyas2104@gmail.com>
2025-07-10 10:39:07 +0800

    change next.config.js to new backend domain

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'razpos.frappe.cloud',
+        hostname: 'razpos.razcraft.com',
         port: '',
         pathname: '/**',
       },
