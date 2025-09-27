3934a248bd353dd65538ee996b55c7f4f2852b8d
ilyas182 <ilyas2104@gmail.com>
2025-09-27 21:13:12 +0800

    add item image to inventory details

--- src/app/components/inventory/InventoryEdit.tsx
+++ src/app/components/inventory/InventoryEdit.tsx
@@ -73,6 +73,51 @@ export default function InventoryDetails({ item, onClose, onUpdate }: InventoryD
             </svg>
           </button>
         </div>
+        <div className="w-full h-48 relative rounded-lg overflow-hidden hover:scale-105 transition-all duration-300"
+        style={{
+          cursor: 'pointer',
+          backgroundImage: `url(${item.image ? `${process.env.NEXT_PUBLIC_API_URL}/${item.image}` : ""})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+        >
+          {/* <SafeImage
+            src={item.image ? `${process.env.NEXT_PUBLIC_API_URL}/${item.image}` : ""}
+            alt={item.item_name}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          /> */}
+        <div className="absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-all duration-300">  
+          <button className="p-2 hover:bg-gray-100 rounded-full"
+          onClick={() => {console.log('delete')}}
+          >
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+            d="M16 6V5.2C16 4.0799 16 3.51984 15.782 3.09202C15.5903 2.71569 15.2843 2.40973 14.908 2.21799C14.4802 2 13.9201 2 12.8 2H11.2C10.0799 2 9.51984 2 9.09202 2.21799C8.71569 2.40973 8.40973 2.71569 8.21799 3.09202C8 3.51984 8 4.0799 8 5.2V6M10 11.5V16.5M14 11.5V16.5M3 6H21M19 6V17.2C19 18.8802 19 19.7202 18.673 20.362C18.3854 20.9265 17.9265 21.3854 17.362 21.673C16.7202 22 15.8802 22 14.2 22H9.8C8.11984 22 7.27976 22 6.63803 21.673C6.07354 21.3854 5.6146 20.9265 5.32698 20.362C5 19.7202 5 18.8802 5 17.2V6"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            />
+            </svg>
+          </button>
+
+          <button className="p-2 hover:bg-gray-100 rounded-full"
+          onClick={() => {console.log('edit')}}
+          >
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 3.99998H6.8C5.11984 3.99998 4.27976 3.99998 3.63803 4.32696C3.07354 4.61458 2.6146 5.07353 2.32698 5.63801C2 6.27975 2 7.11983 2 8.79998V17.2C2 18.8801 2 19.7202 2.32698 20.362C2.6146 20.9264 3.07354 21.3854 3.63803 21.673C4.27976 22 5.11984 22 6.8 22H15.2C16.8802 22 17.7202 22 18.362 21.673C18.9265 21.3854 19.3854 20.9264 19.673 20.362C20 19.7202 20 18.8801 20 17.2V13M7.99997 16H9.67452C10.1637 16 10.4083 16 10.6385 15.9447C10.8425 15.8957 11.0376 15.8149 11.2166 15.7053C11.4184 15.5816 11.5914 15.4086 11.9373 15.0627L21.5 5.49998C22.3284 4.67156 22.3284 3.32841 21.5 2.49998C20.6716 1.67156 19.3284 1.67155 18.5 2.49998L8.93723 12.0627C8.59133 12.4086 8.41838 12.5816 8.29469 12.7834C8.18504 12.9624 8.10423 13.1574 8.05523 13.3615C7.99997 13.5917 7.99997 13.8363 7.99997 14.3255V16Z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            />
+            </svg>
+          </button>
+        </div>
+        </div>
         <div className="flex items-center gap-2 justify-between w-full">
           <span className="text-sm text-gray-500 p-2">Item Code: {item.name}</span>
           <span className="text-lg font-semibold text-gray-700 p-2">${item.price?.price_list_rate.toFixed(2)}</span>
